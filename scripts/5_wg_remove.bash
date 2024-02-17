1b970dc4c598cea000dc47a539201c25dfde8446
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-16 22:54:55 -0800

    Update 5_wg_remove.bash

--- scripts/5_wg_remove.bash
+++ scripts/5_wg_remove.bash
@@ -4,7 +4,7 @@ echo "---------------------------------------------"
 echo "5 Removing Wireguard"
 
 function set_vars(){
-    ADMIN_SERVER="starvpnzone.duckdns.org"
+    ADMIN_SERVER="star-vpn.duckdns.org"
     ADMIN_SERVER_PORT=14533
     WG_SERVER_PUBLIC_KEY=`sudo cat /etc/wireguard/public.key`
     PUBLIC_IP_ADDRESS=`curl https://api.ipify.org`
